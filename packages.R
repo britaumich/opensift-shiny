cb0e49428aa69ac0665ee8c9b72da8ef9d921e48
Chris Stockbridge <crstock@umich.edu>
2021-03-05 08:30:58 -0500

    don't specify folder to install R libraries

--- packages.R
+++ packages.R
@@ -3,7 +3,7 @@ using<-function(...) {
     req<-unlist(lapply(libs,require,character.only=TRUE))
     need<-libs[req==FALSE]
     if(length(need)>0){ 
-        install.packages(need, lib="/usr/local/lib/R/library")
+        install.packages(need)
         lapply(need,require,character.only=TRUE)
     }
 }
